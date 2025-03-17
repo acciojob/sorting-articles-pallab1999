95e9e317bef32aad428281cf5a1e2532382ec1d3
Pallab dutta <121430112+pallab1999@users.noreply.github.com>
2025-03-17 19:06:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
 //your JS code here. If required.
-function stripArticle(name) {
-    return name.replace(/^(a |an |the )/i, '').trim();
-}
-
-const sortedBands = bands.sort((a, b) => stripArticle(a).localeCompare(stripArticle(b)));
-
-document.getElementById('band').innerHTML = sortedBands.map(band => `<li>${band}</li>`).join('');
